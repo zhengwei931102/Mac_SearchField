906c7fd98c8072053ba4778e03e32288e61c3fc5
zhengwei931102 <admin@admindemac-mini.local>
2017-02-08 14:24:18 +0800

    Initial Commit

---
+++ SearchField/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SearchField
+//
+//  Created by admin on 17/2/8.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
